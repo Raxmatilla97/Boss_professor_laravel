e6d253b910c6736f7613d86c97dbfa45bedf1a0b
Raxmatilla <56291918+Raxmatilla97@users.noreply.github.com>
2024-01-18 14:45:33 +0500

    ETst

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -136,7 +136,7 @@
                                     <li>
                                         <a href="#" onclick="openCity(event, 'Moderator{{ $moderator->id }}')"
                                             id="defaultOpen"
-                                            class="tablinks inline-flex items-center px-2 py-3 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600"
+                                            class="tablinks inline-flex items-center pl-4 pr-4 py-3 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600"
                                             aria-current="page">
                                             <svg class="w-4 h-4 me-2 text-white" aria-hidden="true"
                                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
